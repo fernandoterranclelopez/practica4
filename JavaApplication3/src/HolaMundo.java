4b7ec266ffb2c9008dd2cce21d584ca4902452e5
FERNANDO TERRANCLE <ferchuterrancle@gmail.com>
2019-12-14 18:05:07 +0100

    Nueva forma de imprimir Hola Mundo

--- JavaApplication3/src/HolaMundo.java
+++ JavaApplication3/src/HolaMundo.java
@@ -11,7 +11,7 @@
 public class HolaMundo {
     
     public static void main(String [] args){
-        System.out.println("Hola mundo");
+        System.out.println("Nuevo Hola mundo");
     }
     
 }
